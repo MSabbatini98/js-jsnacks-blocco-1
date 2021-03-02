09dd0fda59f786113a087e1b49f16ddf8f44702a
MSebb23 <matteosabbatini23@gmail.com>
2021-03-02 12:27:21 +0100

    Snack 4

--- snack_3/script.js
+++ snack_3/script.js
@@ -2,17 +2,19 @@ var tot = 0;
 var num = 0;
 
 for (var i = 1; i <= 10; i++) {
+    //todo non funziona il validator
     for (var j = 1; j < 10; j++) {
         if(!isNaN(num)){
-            console.log(num);
+            num = parseInt(prompt("inserisci un numero da sommare"));
+            console.log("valore inserito", num);
+            tot += num;
+            console.log(i, num, tot);
             break;
         } else {
+            console.log("valore sbagliato");
             num = parseInt(prompt("Inserisci un NUMERO "));
         }
     }
-    num = parseInt(prompt("inserisci un numero da sommare"));
-    tot += num;
-    console.log(i, num, tot);
 }
 
 document.getElementById("outcome").innerHTML = "la somma di tutti e 10 i valori inseriti è " + tot;
---
+++ snack_4/script.js
@@ -0,0 +1,27 @@
+var invitati = [    "Samuele", 
+                    "Lorena", 
+                    "Francesca", 
+                    "Gabriele", 
+                    "Vito", 
+                    "Luca", 
+                    "Matteo", 
+                    "Michele", 
+                ];
+var esito = false;
+var utente = prompt("Inserisci il tuo nome e scopri se sei invitato anche tu alla festa di Inzaghi. N.B. I nomi propri hanno l'iniziale maiuscola");
+
+for (var i = 0; i < invitati.length; i++) {
+    console.log(i, invitati[i]);
+    if (invitati[i] == utente) {
+        esito = true;
+        break;
+    } else {
+        esito = false;
+    }
+}
+
+if (esito == true) {
+    document.getElementById("outcome").innerHTML = "Sei invitato anche te alla festa di Inzaghi!";
+} else {
+    document.getElementById("outcome").innerHTML = "Mi dispiace, ma non sei abbastanza popular per essere invitato alla festa di Inzaghi!";
+}
