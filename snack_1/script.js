004b2c1a724b1f6a61acd4fff214a7bd85d8b7a5
MSebb23 <matteosabbatini23@gmail.com>
2021-03-02 11:22:13 +0100

    Added validation

--- snack_1/script.js
+++ snack_1/script.js
@@ -3,6 +3,14 @@ var num_2 = prompt("Inserisci il secondo numero ");
 
 var maggiore;
 
+for (var i = 1; i < 10; i++) {
+    if(!isNaN(num_2) && !isNaN(num_2)){
+        console.log(num_1, num_2)
+    } else {
+        num_1 = prompt("Inserisci il primo NUMERO ");
+        num_2 = prompt("Inserisci il secondo NUMERO ");
+    }
+}
 
 if (num_1 > num_2){
     maggiore = num_1;
@@ -11,6 +19,7 @@ if (num_1 > num_2){
 } else if (num_1 < num_2) {
     maggiore = num_2
 }
+
 document.getElementById("input_1").innerHTML = "Il primo input è stato : " + num_1;
 document.getElementById("input_2").innerHTML = "Il secondo input è stato : " + num_2;
 
