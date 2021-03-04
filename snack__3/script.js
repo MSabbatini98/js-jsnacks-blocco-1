c9afa7815674a8ab24b0a29cca29820b592c1c89
MSebb23 <matteosabbatini23@gmail.com>
2021-03-04 12:47:14 +0100

    Snack 3 Blocco 2

---
+++ snack__3/script.js
@@ -0,0 +1,38 @@
+var array_nomi = [
+                    "Andrea",
+                    "Matteo",
+                    "Gabriele",
+                    "Beatrice",
+                    "Davide",
+                    "Alfredo"
+                ];
+
+var array_cognomi = [
+                        "Inzaghi",
+                        "Greco",
+                        "Corti",
+                        "Castagno",
+                        "Sadotti",
+                        "Ficocelli"
+                    ];
+var random_value;
+var random_nomi_cognomi = [];
+
+
+
+for (var yyy = 0; yyy < array_nomi.length; yyy++){
+
+    random_value_nome = Math.floor(Math.random() * array_nomi.length );
+    random_value_cognome = Math.floor(Math.random() * array_cognomi.length ); 
+
+    console.log(random_value_nome, random_value_cognome);
+
+    random_nomi_cognomi.push(array_nomi[random_value_nome] + array_cognomi[random_value_cognome]);
+
+    console.log(array_nomi[random_value_nome], array_cognomi[random_value_cognome]);
+
+}
+
+for (var yy = 0; yy < random_nomi_cognomi.length; yy++){
+    console.log(random_nomi_cognomi[yy]);
+}
