24ea3ab1a1f2a8d785972c7d34815a97f3fe17c7
MSebb23 <matteosabbatini23@gmail.com>
2021-03-04 11:52:57 +0100

    Snack 1 Blocco 2 v2

--- snack__1/script.js
+++ snack__1/script.js
@@ -1,36 +1,31 @@
 var num;
 var somma = 0;
-/*
 // versione con il ciclo for
 for (var i = 0; i < 5; i++){
     // number validator
-    for (var xxx = 10; xxx != 0; xxx--){
-        num = parseInt(prompt("Inserisci un numero intero:"));
-        if (!(isNaN(num)) && num > 0) {
-            break;
-        } else {
-            alert("Attenzione, non ha inserito un numero intero positivo, ritentare")
-        }
-    }
+    num = parseInt(prompt("Inserisci un numero intero positivo: "));
+    while ((isNaN(num) || num <= 0)) {
+        alert ("NON hai inserito un numero intero positivo");
+        num = parseInt(prompt("Inserisci un numero intero positivo:"));
+    } 
+
     somma += num;
     console.log(somma, num);
     document.getElementById("outcome").innerHTML = somma;
 }
 
-*/
+/*
 
 // versione con while
 var j = 0;
 while (j < 5){
     // number validator
-    for (var xxx = 10; xxx != 0; xxx--){
-        num = parseInt(prompt("Inserisci un numero intero:"));
-        if (!(isNaN(num)) && num > 0) {
-            break;
-        } else {
-            alert("Attenzione, non ha inserito un numero intero positivo, ritentare")
-        }
-    }
+    num = parseInt(prompt("Inserisci un numero intero positivo: "));
+    while ((isNaN(num) || num <= 0)) {
+        alert ("NON hai inserito un numero intero positivo");
+        num = parseInt(prompt("Inserisci un numero intero positivo:"));
+    } 
+
 
     somma += num;
     console.log(somma, num);
@@ -38,4 +33,5 @@ while (j < 5){
     j++;
 }
 
+*/
 
